00f84e399b244d1856c4c1f65bf22479f11f962b
Sanjeev Shrestha <sanjeev@joomlaguru.com.np>
2015-03-06 14:26:04 +0545

    Initial Commit of Component

---
+++ component/site/helpers/varnishpurge.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @version     1.0.0
+ * @package     com_varnishpurge
+ * @copyright   Copyright (C) 2015. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      AlchemistHQ <info@alchemisthq.com> - http://www.alchemisthq.com
+ */
+defined('_JEXEC') or die;
+
+class VarnishpurgeFrontendHelper {
+    
+}
